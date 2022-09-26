4f2536bdf57a838da0d687b8d0299828b22a6307
MGamezG <l171130314@gamadero.tecnm.mx>
2022-09-26 13:32:27 -0500

    infiniteScroll

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Movie } from './../../interfaces/cartelera-response';
 import { PeliculasService } from './../../services/peliculas/peliculas.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   moviesList:Movie[]=[]
+  moviesSlice:Movie[]=[]
+  @HostListener('window:scroll',['$event'])
+  onScroll(){
+    const positionScroll=(document.documentElement.scrollTop||document.body.scrollTop)+1000;
+    const maxPositionScroll=(document.documentElement.scrollHeight||document.body.scrollHeight)
+    if (positionScroll>maxPositionScroll) {
+      this.peliculasService.getMovies().subscribe(
+        response=>{
+          this.moviesList.push(...response.results)
+        }
+      );
+    }
+    console.log({positionScroll,maxPositionScroll})
+  }
 
   constructor(private peliculasService:PeliculasService) {
     peliculasService.getMovies().subscribe(
       data=>{
         //console.log(data)
+        //console.log(data)
+        this.moviesSlice=data.results
         this.moviesList=data.results
       }
     )
--- src/app/services/peliculas/peliculas.service.ts
+++ src/app/services/peliculas/peliculas.service.ts
@@ -1,18 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable}from 'rxjs'
+import {Observable, tap}from 'rxjs'
 import { CarteleraResponse } from 'src/app/interfaces/cartelera-response';
 @Injectable({
   providedIn: 'root'
 })
+//otro key=402c75d67b34f271b9134f1d0c39494
 export class PeliculasService {
-
+  baseUrl='https://api.themoviedb.org/3'
+  carteleraPage=1
   constructor(private http:HttpClient) { }
+  get params(){
+    return {
+      'api_key':'e48588d3c844b39f7314a620a6407d50',
+      'language':'es-ES',
+      'page':this.carteleraPage
+    }
+  }
 
   getMovies():Observable<CarteleraResponse>{
-    return this.http.get<CarteleraResponse>(`https://api.themoviedb.org/3/movie/now_playing?api_key=402c75d67b34f271b9134f1d0c394948&language=es-ES&page=1`)
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`,{params:this.params}).pipe(
+      tap(()=>{
+        this.carteleraPage+=1
+      })
+    )
   }
   getMovies2(page:number):Observable<CarteleraResponse>{
-    return this.http.get<CarteleraResponse>(`https://api.themoviedb.org/3/movie/now_playing?api_key=402c75d67b34f271b9134f1d0c394948&language=es-ES&page=${page}`)
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing?api_key=e48588d3c844b39f7314a620a6407d50&language=es-ES&page=${page}`)
   }
 }
