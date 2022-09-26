f80e4b89b28da17459b66a0a1577705864324142
MGamezG <l171130314@gamadero.tecnm.mx>
2022-09-26 09:28:51 -0500

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { BuscarComponent } from './pages/buscar/buscar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import {PaliculaComponent}from './pages/palicula/palicula.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'pelicula/:id',
+    component:PaliculaComponent
+  },
+  {
+    path:'search/:text',
+    component:BuscarComponent
+  },
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
