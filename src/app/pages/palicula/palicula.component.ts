6b6fec4f367b1560a93758a48bb94b1b96e4df52
MGamezG <l171130314@gamadero.tecnm.mx>
2022-09-30 09:42:36 -0500

    desestructuracion

--- src/app/pages/palicula/palicula.component.ts
+++ src/app/pages/palicula/palicula.component.ts
@@ -1,3 +1,4 @@
+import { Cast } from './../../interfaces/creditsResponse';
 import { Movie } from './../../interfaces/cartelera-response';
 import { MovieResponse } from './../../interfaces/movieResponse';
 import { PeliculasService } from './../../services/peliculas/peliculas.service';
@@ -5,6 +6,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { CreditsResponse } from 'src/app/interfaces/creditsResponse';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-palicula',
@@ -23,27 +25,45 @@ export class PaliculaComponent implements OnInit {
   ngOnInit(): void {
     const id=this.activatedRoute.snapshot.params['id']
     console.log(id)
-    this.peliculasService.getMovieDetail(id).subscribe(
-      (data)=>{
-        if(!data){
+    combineLatest([
+      this.peliculasService.getMovieDetail(id),
+      this.peliculasService.getCredits(id)
+    ]).subscribe(
+      ([pelicula,Cast])=>{//desestructuracion de argumentos
+        console.log(pelicula,Cast)
+        if(!pelicula){
           this.router.navigateByUrl('/home')
           return ;
         }
         else{
-          console.log(data)
-        this.Movie=data
+          console.log(pelicula)
+        this.Movie=pelicula
+        this.cast=Cast.filter(
+                 actor=>actor.profile_path!=null
+               )
         }
+      });
+    // this.peliculasService.getMovieDetail(id).subscribe(
+    //   (data)=>{
+        // if(!data){
+        //   this.router.navigateByUrl('/home')
+        //   return ;
+        // }
+        // else{
+        //   console.log(data)
+        // this.Movie=data
+        // }
 
-      }
-    );
-    this.peliculasService.getCredits(id).subscribe(
-      (cast)=>{
-        console.log(cast)
-        this.cast=cast.filter(
-          actor=>actor.profile_path!=null
-        )
-      }
-    );
+    //   }
+    // );
+    // this.peliculasService.getCredits(id).subscribe(
+    //   (cast)=>{
+    //     console.log(cast)
+    //     this.cast=cast.filter(
+    //       actor=>actor.profile_path!=null
+    //     )
+    //   }
+    // );
   }
   onRegresar(){
     this.location.back()
